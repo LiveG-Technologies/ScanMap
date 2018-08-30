367a906d8a0d55d420a0aaa92ef1b72ee019847f
LiveG <hi@liveg.tech>
2018-08-30 15:31:29 +0100

    Add scanner UI

--- src/script.js
+++ src/script.js
@@ -1,10 +1,29 @@
 var scanner;
+var camera = 0;
+
+function changeCamera() {
+    Instascan.Camera.getCameras().then(function(cameras) {
+        camera++;
+        
+        if (camera >= cameras.length) {camera = 0;}
+
+        scanner.start(cameras[camera]);
+    });
+}
 
 $(function() {
     scanner = new Instascan.Scanner({video: $("#preview")[0]});
 
     scanner.addListener("scan", function(content) {
-        alert(content);
+        $("#lens").css("background-color", "rgba(95, 237, 83, 0.4)");
+
+        setTimeout(function() {
+            $("#lens").css("background-color", "rgba(255, 255, 255, 0.4)");
+
+            setTimeout(function() {
+                alert(content);
+            }, 500);
+        }, 500);
     });
 
     Instascan.Camera.getCameras().then(function(cameras) {
